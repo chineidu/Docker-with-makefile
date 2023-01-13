35ab5e499e89de4899af9cefdf81b460a1674f66
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-01-13 14:48:33 +0100

    deleted:    docs_tutorial/app.py 	modified:   pyproject.toml 	modified:   requirements.txt 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
-from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+from pathlib import Path
+
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
+
+style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1", "pylint==2.15.10"]
+
+test_packages = ["pytest==7.1.2", "pytest-cov==2.10.1", "evidently==0.2.1"]
+
+# Define our package
+setup(
+    name="simple-project",
+    version=0.1,
+    description="Simple Project Development With Docker.",
+    author="Chinedu Ezeofor",
+    author_email="neidu@email.com",
+    url="https://github.com/chineidu/Docker-with-makefile",
+    python_requires=">=3.8",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+    extras_require={
+        "dev": docs_packages + style_packages + test_packages + ["pre-commit==2.19.0"],
+        "docs": docs_packages,
+        "test": test_packages,
+    },
+)
